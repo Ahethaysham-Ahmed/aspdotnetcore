96f7d282f395431ea13c1afa04a601dec170825b
Ahethaysham Ahmed <69258624+ahethaysham@users.noreply.github.com>
2023-05-08 20:10:18 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,25 +1,29 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var builder = WebHost.CreateDefaultBuilder(args)
+var builder = Host.CreateDefaultBuilder(args)
     .ConfigureWebHostDefaults(webBuilder =>
     {
         webBuilder.UseStartup<Startup>();
+
+        if (!webBuilder.GetSetting("Environment").Equals("Development"))
+        {
+            webBuilder.UseExceptionHandler("/Error");
+        }
+        webBuilder.UseStaticFiles();
+
+        webBuilder.UseRouting();
+
+        webBuilder.UseAuthorization();
+
+        webBuilder.UseEndpoints(endpoints =>
+        {
+            endpoints.MapRazorPages();
+        });
     });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-}
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
 
 app.Run();
